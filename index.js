b0da5337c2eee1866ac9b1eff96597035615b1f7
Kat Berge <kat.n.berge@gmail.com>
2020-09-13 22:45:00 -0500

    makes the buttons work as links (instead of having an <a> tag inside a button with no functionality)

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ window.addEventListener("hashchange", () => {
     if (location.hash === "#project-section") {
         window.scrollBy(0, -50);
     }
-})
+});
+
+const openLink = (link) => {
+    window.open(`http://${link}`, '_target');
+};
